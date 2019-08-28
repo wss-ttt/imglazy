c9121e38939495d5c4f3d2504c19784705734703
王水胜 <602466011@qq.com>
2019-08-28 16:03:51 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+
+import VueLazyLoad  from 'vue-lazyload'
+Vue.use(VueLazyLoad, {
+  error: require('../static/error.jpg'),
+  loading: require('../static/loading.gif'),
+  attempt: 1
+})
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/views/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <button @click="getData">请求数据</button>
+    <ul>
+      <li v-for="(item,index) in list" :key="index">
+        <!-- <img :src="imgProxy(item.image)" alt /> -->
+        <img v-lazy="imgProxy(item.image)" alt />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { imgProxy } from '@/utils/index.js'
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      list: []
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    imgProxy,
+    getData() {
+      this.$http({
+        url: '/api/4/news/latest',
+        method: 'get'
+      }).then(({ data }) => {
+        this.list = data.top_stories
+      })
+    }
+  },
+  created() {},
+  mounted() {}
+}
+</script>
+<style scoped>
+ul,
+li {
+  margin: 0;
+  list-style-type: none;
+}
+ul li img {
+  width: 120px;
+  height: 120px;
+}
+</style>
